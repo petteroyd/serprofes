b7570c2e786a05bff44e036a3e1bac667ead9838
petteroyd <borreropm@gmail.com>
2026-05-11 21:56:44 +0200

    Cesta frutas

---
+++ 20260511/01-Ejercicio/script.js
@@ -0,0 +1,4 @@
+const colores = ["Yellow",  "Red", "Black"];
+console.log(colores);
+console.log(colores[0]);
+
---
+++ 20260511/02-Ejercicio/script.js
@@ -0,0 +1,16 @@
+// Creamos nuestra "Caja grande" (Array)
+let carrito = ["🍎 Manzana","🍍 Piña"];
+// Imprimir el carrito nada mas al cargar la página
+document.getElementById("pantalla_cesta").textContent = carrito.join(" - ");
+
+// La función para agregar cosas nuevas a la lista
+function añadir_al_carrito(){
+    // Atrapamos lo que escribe el usuario
+    let item = document.getElementById("nuevo_item").value;
+    // Magia de arrays: .push() mete el nuevo elemento al final de la lista
+    carrito.push(item);
+    // Volvemos a pintar la cesta para que se vea
+    document.getElementById("pantalla_cesta").textContent = carrito.join(" - ");
+    // Limpiamos el input
+    document.getElementById("nuevo_item").value = "";
+}
